9fbaf5aee3ee69477250a73344bb066f5499caaa
qiulinqu <121984536+qiulinqu@users.noreply.github.com>
2023-08-16 21:26:20 -0700

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -19,14 +19,17 @@ import matplotlib.pyplot as plt
 # question 5 machine learning
 def train_and_evaluate_model(df, features, target):
     '''
-    Return the accuracy and mean square error of popularity of songs, predicting through song features.
-    Plot and calculate the correlation between Predicted Popularity and Actual Popularity
+    Return the accuracy and mean square error of
+    popularity of songs, predicting through song features.
+    Plot and calculate the correlation between Predicted
+    Popularity and Actual Popularity
     '''
 
     X = df[features]
     y = df[target]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.2,
+                                                        random_state=42)
 
     model = LinearRegression()
     model.fit(X_train, y_train)
@@ -37,7 +40,7 @@ def train_and_evaluate_model(df, features, target):
     print(f"Mean Squared Error (MSE): {mse:.2f}")
 
     correlation, _ = pearsonr(np.array(y_test['popularity']), y_pred.flatten())
-    print(f"Correlation between Predicted Popularity and Actual Popularity: {correlation:.2f}")
+    print(f"Correlation of Predicted Actual Popularity: {correlation:.2f}")
 
     plt.scatter(y_test, y_pred)
     plt.xlabel("Actual Popularity")
@@ -45,10 +48,12 @@ def train_and_evaluate_model(df, features, target):
     plt.title("Actual Popularity vs. Predicted Popularity")
     plt.show()
 
+
 if __name__ == '__main__':
     df = pd.read_csv('/content/top50MusicFrom2010-2019.csv')
     # machine learning
-    features = ['duration', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness',
+    features = ['duration', 'danceability', 'energy', 'loudness',
+                'speechiness', 'acousticness',
                 'liveness', 'valence', 'tempo']
     target = ['popularity']
 
